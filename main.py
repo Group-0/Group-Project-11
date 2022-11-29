1a9075f387259998b29480828dcdbb7490285978
Irish Avorque <irish.avorque@tamu.edu>
2022-11-28 21:06:10 -0600

    Update main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import subprocess
+import argparse
+import getpass
+import logging
+import WebDriver
+
+CMD_LIST = {
+	'md5': 'Return a MD5 hash of an input string',
+	'factorial': 'Return the factorial of an input integer',
+    'fibonnaci_sequence': 'Display an array of integers that are less than or equal to the input number',
+    'is-prime': 'Determines if input number is prime or not prime',
+    'slack_alert': 'Post input on Slack'
+}
+
+def md5():
+
+def factorial():
+
+def fibonnaci():
+
+def is-prime():
+
+def slack_aler():
